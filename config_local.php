2b265d3e247bbf346a74904b6ac39b7dc272a8d8
hankawa-takashi <w7deca7288s7h7q@docomo.ne.jp>
2021-07-25 16:09:29 +0900

    kjhyu

--- config_local.php
+++ config_local.php
@@ -4,9 +4,9 @@ ini_set('display_errors',1);
 //日本時間に設定
 date_default_timezone_set('Asia/Tokyo');
 //URL/ディレクトリ設定
-define('HOME_URL','https://nice-biedy.com/TwitterClone/');
+define('HOME_URL','/TwitterClone/');
 //データーベースの接続情報
 define('DB_HOST', '127.0.0.1');
-define('DB_USER', 'dbuser');
-define('DB_PASSWORD', 'Hankawa-5910');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'twitter_clone');
